9ce7e6733c0cdea5f76aad4248bf71ec0756e1f5
popeye-api <70052083+popeye-api@users.noreply.github.com>
2020-10-29 16:10:37 +0700

    Update linebot_basic.php

--- linebot_basic.php
+++ linebot_basic.php
@@ -15,6 +15,7 @@ foreach ($request_json['events'] as $event)
 			$text = $event['message']['text'];
 			
 			$reply_message = 'ฉันได้รับข้อความ '. $text.' ของคุณแล้ว!';   
+			$reply_message = 'Popeye';   
 			
 		} else {
 			$reply_message = 'ฉันได้รับ '.$event['message']['type'].' ของคุณแล้ว!';
