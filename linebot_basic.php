528d7fb5f05128be872f5b10773eeff937964dac
popeye-api <70052083+popeye-api@users.noreply.github.com>
2020-11-17 16:59:27 +0700

    Update linebot_basic.php

--- linebot_basic.php
+++ linebot_basic.php
@@ -15,7 +15,7 @@ foreach ($request_json['events'] as $event)
 			$text = $event['message']['text'];
 			$test = explode(" ", $text);
 			$reply_message = 'ฉันได้รับข้อความ '. $text.' ของคุณแล้ว!';   
-			$reply_message = 'Popeye'; 
+			$reply_message = 'Popeye'." ".$test[3]; 
 			//$reply_message = mySQL_selectAll('http://bot.kantit.com/json_select_users.php');
 			
 			if($text == "@บอท ฉันต้องการค้นหาข้อมูลนิสิตทั้งหมด"){
